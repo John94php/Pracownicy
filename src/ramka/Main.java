0ea75f9acf4568699fb0cfdcad085002c97aa5aa
Johny <janzalesinski@gmail.com>
2021-05-03 17:57:33 +0200

    change app layout

--- src/ramka/Main.java
+++ src/ramka/Main.java
@@ -7,62 +7,47 @@ import java.awt.event.ActionListener;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-public class Main  extends JFrame {
+public class Main extends JFrame {
     public Main() {
-
-        int szer = Toolkit.getDefaultToolkit().getScreenSize().width;
-        int wys = Toolkit.getDefaultToolkit().getScreenSize().height;
-        this.setSize(szer / 2, wys / 4 );
-        int szerRamki = this.getSize().width;
-        int wysRamki = this.getSize().height;
-        this.setLocation((szer - szerRamki) / 2, (wys - wysRamki) / 2);
         initComponents();
-        this.setTitle("Pracownicy");
-        this.setResizable(false);
-        this.setDefaultCloseOperation(3);
     }
 
     public void initComponents() {
+        JLabel czas = new JLabel(pobierzCzas());
+        czas.setFont(new Font("Monospaced", Font.ITALIC, 15));
 
-        add = new JButton("Dodaj pracownika");
-        list = new JButton("Lista pracowników");
-        edit = new JButton("Edytuj pracownika");
-        add.setLocation(10, 25);
-
-        list.setLocation(215, 25);
-        edit.setLocation(420, 25);
-        add.setSize(200, 30);
-        list.setSize(200, 30);
-        edit.setSize(200, 30);
-        container.add(add);
-        container.add(list);
-        container.add(edit);
-        container.add(czas);
-        czas.setFont(new Font("Monospaced",Font.ITALIC,15));
-        add.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Akcja");
-            }
-        });
         ActionListener stoper = new Zegar();
         Timer zegar = new Timer(1000, stoper);
         zegar.start();
+        this.setTitle("Pracownicy");
+        this.setBounds(300, 300, 300, 200);
+        panelMenu.setLayout(new GridLayout(5, 1));
+        panelMenu.add(menu1);
+        panelMenu.add(menu2);
+        panelMenu.add(menu3);
+        panelMenu.add(menu4);
+        panelMenu.add(czas);
+        container.add(panelMenu);
+        this.setDefaultCloseOperation(3);
 
     }
 
-    JButton add;
-    JButton list;
-    JButton edit;
-    JRadioButton M = new JRadioButton("Mężczyzna", false);
-    JRadioButton K = new JRadioButton("Kobieta", false);
-    Container container = this.getContentPane();
-    JLabel czas = new JLabel(pobierzCzas());
+    private final Container container = this.getContentPane();
+    private final JPanel panelMenu = new JPanel();
+    private final MenuButton menu1 = new MenuButton("Dodaj pracownika");
+    private final MenuButton menu2 = new MenuButton("Lista pracownikow");
+    private final MenuButton menu3 = new MenuButton("Usuń pracownika");
+    private final MenuButton menu4 = new MenuButton("Edytuj pracownika");
+
+    private class MenuButton extends JButton {
+        public MenuButton(String nazwa) {
+            super(nazwa);
+        }
+    }
 
     private class Zegar implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            czas.setText(pobierzCzas());
         }
     }
 
